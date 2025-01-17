2a9b726c26a75a9d8727f67b9d53b785f288acdf
Arebamen <gabquelyn@gmail.com>
2025-01-17 15:47:25 +0000

    added the routes for forget and reset password

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,11 +4,15 @@ import {
   loginController,
   logoutController,
   refreshController,
+  forgotPasswordController,
+  restPasswordController
 } from "../controllers/authController";
 const authRouter = Router();
 authRouter.route("/").post(loginController);
 authRouter.route("/verify").post(verifyController);
 authRouter.route("/logout").post(logoutController);
 authRouter.route("/refresh").post(refreshController);
+authRouter.route("/forgot").post(forgotPasswordController)
+authRouter.route("/reset").post(restPasswordController)
 // authRouter.route("/register").post(registerController);
 export default authRouter
